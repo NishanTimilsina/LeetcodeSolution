ec01998005ed9a50a81c998d8e13914850cc0d64
Nishan Timilsina <timilnishan@gmail.com>
2020-02-13 15:59:15 -0500

    Update LongestCommonPrefix.java

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -22,3 +22,68 @@ class Solution {
         return longestCommonPrefix;
     }
 }
+/********************** next solution ****************/
+//fast solution
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+           
+        String commonpre= "";
+
+        if(strs==null || strs.length<=0){
+            return commonpre;
+        }
+        int first = 0;
+        int min = getMinString(strs);
+        int last = min;
+
+
+        while(first<=last){
+            
+            int mid = first+(last-first)/2;
+            
+            if(isLongestprefix(strs,strs.length,strs[0],first,mid)){
+            
+                
+                //add prefix
+                commonpre += strs[0].substring(first,mid+1);
+                
+                first = mid+1;
+                
+            }else{
+                last = mid-1;
+            }
+            
+        }
+        
+        return commonpre;
+    }
+    
+    public boolean isLongestprefix(String[] strs,int n,String str,int start,int end){
+        
+        for(int i=0;i<strs.length;i++){
+            
+            String value = strs[i];
+            
+            for(int j=start;j<=end;j++){
+                
+                if(value.charAt(j)!=str.charAt(j))
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    public int getMinString(String[] str){
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(int i=0;i<str.length;i++){
+            
+            if(str[i].length()<min){
+                min = str[i].length();
+            }
+        }
+        return min;
+    }
+}
