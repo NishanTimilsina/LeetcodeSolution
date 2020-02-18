3ab2ca49b749114323df1c9797b99796760d3132
Nishan Timilsina <timilnishan@gmail.com>
2020-02-17 20:18:47 -0500

    Update Top K Frequent Words.java

--- Top K Frequent Words.java
+++ Top K Frequent Words.java
@@ -45,3 +45,41 @@ class Solution {
         return result;
     }
 }
+
+
+///****************** compile code
+
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        if (words == null || words.length == 0) {
+            return new ArrayList<String>();
+        }
+
+        Map<String, Integer> map = new HashMap();
+        for (String s: words) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        List<String>[] bucket = new List[words.length + 1];
+        for (Map.Entry<String, Integer> set : map.entrySet()) {
+            int frequecny = set.getValue();
+            if (bucket[frequecny] == null) {
+                bucket[frequecny] = new ArrayList<>();
+            }
+            bucket[frequecny].add(set.getKey());
+        }
+
+        List<String> rst = new ArrayList<>();
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] != null) {
+                // Sort the string to meeting demand.
+                Collections.sort(bucket[i]);
+                // We can use addAll method to avoid adding too much elements (exceeding k).
+                for (int j = 0; j < bucket[i].size() && rst.size() < k; j++) {
+                    rst.add(bucket[i].get(j));
+                }
+            }
+        }
+        return rst;
+    }
+}
