d413b5045d070506650449346ea23aabaaf45a5c
Nishan Timilsina <timilnishan@gmail.com>
2020-02-24 11:38:14 -0500

    Create Best Time to Buy and Sell Stock II.java

---
+++ Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+class Solution {
+    public int maxProfit(int[] prices) {
+     
+        int maxProfit = 0;
+        for(int i=0;i<prices.length-1;i++){
+            
+            if(prices[i+1]>prices[i]){
+                
+                int diff = prices[i+1]-prices[i];
+                maxProfit += diff;
+            }
+        }
+        return maxProfit;
+    }
+}
